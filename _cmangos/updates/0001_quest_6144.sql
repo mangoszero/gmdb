2a4bc1f6bd4ab791768703347d4671eb33c4a217
time-out <time-out@gmx.de>
2014-08-22 22:54:47 +0200

    locales quest 6144

---
+++ _cmangos/updates/0001_quest_6144.sql
@@ -0,0 +1,4 @@
+-- https://web.archive.org/web/20071103063417/http://wow.allakhazam.com/db/quest.html?wquest=6144
+UPDATE `locales_quest` 
+SET `Details_loc3` = 'Der Befehl ist gekommen, $C. Varimathras selbst hat verlangt, dass ich meine \"tüchtigsten\" Agenten auf eine höchst diffizile taktische Mission nach Unterstadt schicke.$B$BLeider wurden meine tüchtigsten Agenten alle vor drei Jahren getötet. An ihrer Stelle habe ich eine Bande hirntoter Trottel.$B$B<Nathanos schaut Euch kalt an.>$B$BReist sofort nach Unterstadt und erstattet Varimathras Bericht. Macht mir keine Schande, $C!',`Objectives_loc3` = 'Reist nach Unterstadt und sprecht mit Varimathras.'
+WHERE `entry` = 6144;
